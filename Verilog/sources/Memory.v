5a1dbd3191cea387e24f054a9cc6620070019e14
kchabeli688 <kchabeli688@gmail.com>
2024-05-04 18:04:57 +0200

    updated memory.v file

--- Verilog/sources/Memory.v
+++ Verilog/sources/Memory.v
@@ -1,10 +1,43 @@
-//---------------------------------------------------------------------------//
-//	A 4MB memory module for the Median Filter Implementation.				 //
-//	The memory is implemented in BIG EDIAN									 //									 	
-// 																			 //	
-// 																			 //
-//@Auhtor: Kananelo Chabeli													 //
-//---------------------------------------------------------------------------//
+
+/**
+	@brief: Describes an embedded memory to be used for Mediam filter.
+
+	@detailed description:
+
+	This module defines a 4MB memory ( by default) that stores image pixels for median filter algorithm.
+
+	The following are specifications ( defualt):
+	-> Memory Size: 4Mb
+	-> Memory Data width: 8-bits ( 1 byte)
+	-> Memory Address Bus width: 32-bites (4 bytes)
+	-> Data width: 24-bits ( 3-bytes)
+
+	Working Principle:
+	The memory is written such that is can suppor simuleneous write and read( at different addresses). 
+	**************************READ OPERATION********************************
+	The read operation is done on each positive clock edge when "Mem_Read_Enable" line is set. It done in the following:
+	1. Grab first byte from memory at address 'Mem_Read_Address', the second bytes from 'Mem_Read_Address+1', and 
+	third bytes from 'Mem_Read_Address'+2. 
+	2.The first byte is RED comeponet, second is GREEN,and third is BLUE.
+	3.As such these bytes are shifted such that they form a 24-bit data ( pixel value), which is then written to 'Mem_Output_Data' data bus(output port)
+
+	*************************WRITE OPERATION********************************
+	The write operation is initaited during each positive clock,and when 'Mem_Write_Enable' line is HIGH.
+	It is done in the following steps:
+	1. Grab 24-bit data from 'Mem_Input_Data'.
+	2. upper byte if RED, middle byte is GREEN and lower byte is BLUE.
+	3. Write each byte in the contigious memory locations start at the address given by 'Mem_Write_Address' bus.
+
+	*************************RESET OPERATIONS******************************
+
+	The module is reset by basically keeping 'Mem_Reset' line HIGH for each positive clock. That means for each
+	positive clock the module reset the byte to 0. ( Mem_Reset) should be left long enough to set all bytes  (4194304 by default)to zero. ( will probably take 4194304 clock cycles).
+
+
+
+	@author: Kananelo
+
+*/
 
 
 module Memory(Mem_Clk, Mem_Reset, Mem_Read_Enable, Mem_Write_Enable, Mem_Input_Data,Mem_Write_Address, Mem_Read_Address, Mem_Output_Data);
---
+++ Verilog/testbenches/tb.v
@@ -0,0 +1,1 @@
+//Some testbench------------delete this file!
